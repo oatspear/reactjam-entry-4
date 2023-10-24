d15218215020507aff63c693e8400265c56466a4
oatspear <oatspear@gmail.com>
2023-10-24 21:04:45 +0100

    WIP UI

--- src/components/Army.tsx
+++ src/components/Army.tsx
@@ -12,6 +12,47 @@ interface ArmyProps {
 }
 
 
+function extrasTop(icon: string, value: number): JSX.Element {
+  return (
+    <>
+      { indicators() }
+      { valueLabel(icon, value) }
+    </>
+  );
+}
+
+
+function extrasBottom(icon: string, value: number): JSX.Element {
+  return (
+    <>
+      { valueLabel(icon, value) }
+      { indicators() }
+    </>
+  );
+}
+
+
+function indicators(): JSX.Element {
+  return (
+    <div className="indicators">
+
+    </div>
+  );
+}
+
+
+function valueLabel(icon: string, value: number): JSX.Element {
+  return (
+    <div className="stats">
+      <div className="round-icon icon-32">
+        <img src={icon} alt="army type" />
+      </div>
+      <span>00</span>
+    </div>
+  );
+}
+
+
 const Army = ({ army, flip }: ArmyProps): JSX.Element => {
   const icon: string = army.type === MinionType.POWER
     ? iconPower
@@ -19,15 +60,17 @@ const Army = ({ army, flip }: ArmyProps): JSX.Element => {
     ? iconSpeed
     : iconTechnical;
 
+  const armyValue: number = army.minions * army.tier;
+
   return (
     <div className="army">
-      { flip && <span>stats</span> }
+      { flip && extrasTop(icon, armyValue) }
 
-      <div className="sprite">
+      <div className="round-icon icon-64">
         <img src={icon} alt="sprite" />
       </div>
 
-      { !flip && <span>stats</span> }
+      { !flip && extrasBottom(icon, armyValue) }
     </div>
   );
 };
